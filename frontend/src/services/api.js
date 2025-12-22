790e35560b326a4159e1ad4c3bfe1a05f93c0299
unknown <Paritosh Jaiswal>
2025-12-22 20:28:52 +0530

    deploying ...

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || "http://localhost:8000/api",
+  baseURL: import.meta.env.VITE_API_BASE,
   timeout: 15000,
 });
 
@@ -11,6 +11,6 @@ api.interceptors.request.use((config) => {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
-}); 
+});
 
 export default api;
