725690505512b08b9dcbb5c317e7c505d12eb9a9
unknown <Paritosh Jaiswal>
2025-12-22 20:48:01 +0530

    New one

--- backend/index.js
+++ backend/index.js
@@ -12,9 +12,12 @@ const userRoutes = require("./routes/userRoutes");
 app.use(
   cors({
     origin: ["http://localhost:5173", "https://creator-hub-82iz.vercel.app"],
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
